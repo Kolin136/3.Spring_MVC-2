7723677cf9685e4a6ea80247b26567dbcf507507
Kolin136 <jsowb136@gmail.com>
2024-06-11 16:17:30 +0900

    "섹션6-8~9"

---
+++ 3.login/src/test/java/hello/login/web/session/SessionManagerTest.java
@@ -0,0 +1,41 @@
+package hello.login.web.session;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import hello.login.domain.member.Member;
+import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+class SessionManagerTest {
+
+  SessionManager sessionManager = new SessionManager();
+
+  @Test
+  public void sessionTest() throws Exception {
+    //세션 생성
+    MockHttpServletResponse response = new MockHttpServletResponse();
+    Member member = new Member();
+    sessionManager.createSession(member,response);
+
+    //요청에 응답 쿠키 저장
+    MockHttpServletRequest request = new MockHttpServletRequest();
+    request.setCookies(response.getCookies()); //mySessionId=125325-dfhdhfdgdf-1241
+
+    //세션 조회
+    Object result = sessionManager.getSession(request);
+    assertThat(result).isEqualTo(member);
+    //세션 만료
+    sessionManager.expire(request);
+    Object expired = sessionManager.getSession(request);
+    assertThat(expired).isNull();
+
+  }
+
+
+
+
+}
